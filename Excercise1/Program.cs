3f294f7420857f044f2bf58b26092f8ec9834a5c
Peter Mladenov <53867514+pMladenov90@users.noreply.github.com>
2019-08-08 07:18:17 -0700

    Add project files.

---
+++ Excercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good day!");
+            Console.WriteLine("My name is Peter Mladenov Minchev");
+        }
+    }
+}
