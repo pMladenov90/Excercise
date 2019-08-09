78214241d657d2517c3bd9e7b2f15f4cdc653d8a
Peter Mladenov <53867514+pMladenov90@users.noreply.github.com>
2019-08-09 02:54:20 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
+
+
+        }
+
+       
+    }
+}
